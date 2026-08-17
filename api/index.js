b2b7fe7f5cd6c8974996f2156a288fc64b353ae3
rameezuetian <muhammadrameez262@gmail.com>
2026-08-17 17:41:42 +0500

    Create the middleware the custom error

--- api/index.js
+++ api/index.js
@@ -48,3 +48,16 @@ app.listen(port, () => {
 // api routes
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
+
+
+
+// middle ware
+app.use((err  , req, res , next)=>{
+  const statusCode  = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode ,
+    message,
+  });
+});
