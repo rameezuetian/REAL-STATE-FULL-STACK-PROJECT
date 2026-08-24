bbae86f6ba93d27715b724659bccfb82857fb043
rameezuetian <muhammadrameez262@gmail.com>
2026-08-24 21:23:58 +0500

    Update Header Search Form

--- client/client/src/components/Header.jsx
+++ client/client/src/components/Header.jsx
@@ -2,11 +2,32 @@ import React from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 
 export default function Header() {
-  const {currentUser} = useSelector(state => state.user) 
+  const {currentUser} = useSelector(state => state.user); 
+  const [searchTerm , setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm' ,searchTerm);
+
+    const searchQuery = urlParams.toString();
+    navigate('/search?${searchQuery}')
+  };
+
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTermFormUrl = urlParams.get('searchTerm');
+    if(searchTermFormUrl){
+      setSearchTerm(searchTermFormUrl);
+    }
+  })
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -20,14 +41,19 @@ export default function Header() {
         </Link>
 
         {/* Search */}
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form  onSubmit ={handleSubmit}   className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange = {(e)=> setSearchTerm(e.target.value)}
           />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
 
-          <FaSearch className="text-slate-600" />
+          
         </form>
 
         {/* Navigation */}
